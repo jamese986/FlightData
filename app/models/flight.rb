351a6762e8fd5850ddf1dc5e6088f8ade1a90ccb
jamese986 <james.top986@gmail.com>
2019-01-27 21:11:47 +0000

    updating add flight forms

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,7 +1,23 @@
 class Flight < ApplicationRecord
+  validate :price_range_limit
+
   paginates_per 10
 
   has_one  :airline
   has_many :passengers
 
+  private
+
+  def price_range_limit
+    if price == 0
+      error.add(:price, "cannot be 0")
+    end
+  end
+
+
+  def passenger_booking_limit
+    if passenger.exists? > 1
+      error.add(:passenger, "can only be booked once per flight")
+    end
+  end
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,4 +1,15 @@
 class Passenger < ApplicationRecord
 
   has_many :flights
+
+
+  private
+
+  def price_range_limit
+    if price == 0
+      error.add(:price, "cannot be abover 10,000")
+    else price = 0
+      error.add(:price, "cannot be 0")
+    end
+  end
 end
